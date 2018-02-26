6d368fb45e260638523fda90fa2ee863f6050732
Kevin Li <kevinli@dwightshrute.home>
2018-02-26 03:26:05 -0500

    finished work 04

--- restaurants.py
+++ restaurants.py
@@ -20,3 +20,24 @@ def get_zip_score(zipcode, score):
     return collection.find( {'address.zipcode': zipcode, 'grades.score': {'$lt': score} } )
 
 #Something more clever.
+def get_zip_cuisine(zipcode, cuisine):
+    return collection.find({'address.zipcode': zipcode, 'cuisine': cuisine})
+
+def display_restaurants(collection):
+    for i in collection:
+        print i['name']
+
+print "-----RESTAURANTS IN QUEENS-----\n"
+display_restaurants(get_borough("Queens"))
+
+print "-----RESTAURANTS IN 11101----\n"
+display_restaurants(get_zipcode("11101"))
+
+print "-----RESTAURANTS IN 11101 WITH GRADE A----\n"
+display_restaurants(get_zip_grade("11101", "A"))
+
+print "-----RESTAURANTS IN 11101 WITH SCORE LOWER THAN 7----\n"
+display_restaurants(get_zip_score("11101", 7))
+
+print "-----RESTAURANTS IN 11101 WITH CHINESE CUISINE----\n"
+display_restaurants(get_zip_cuisine('11101','Chinese'))
